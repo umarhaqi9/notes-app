0e357033d59b5a8441eb18590cd83e3b9f89be6d
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2022-11-18 12:51:19 +0700

    initial commit

---
+++ app/root.jsx
@@ -0,0 +1,94 @@
+const {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  Link,
+  useCatch,
+} = require("@remix-run/react");
+
+import styles from '~/styles/main.css';
+import MainNavigation from "~/components/MainNavigation";
+
+export const meta = () => ({
+  charset: "utf-8",
+  title: "Umar's Notes",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <header>
+          <MainNavigation/>
+        </header>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function CatchBoundary(){
+   const caughtResponse = useCatch();
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>{caughtResponse.statusText}</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation/>
+        </header>
+        <main className='error'>
+          <h1>{caughtResponse.statusText}</h1>
+          <p>{caughtResponse.data?.message || 'Something went wrong!'}</p>
+          <p>Back to <Link to="/">safety</Link>!</p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({error}){
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occured!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation/>
+        </header>
+        <main className='error'>
+          <h1>An error occured!</h1>
+          <p>{error.message}</p>
+          <p>Back to <Link to="/">safety</Link>!</p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function links(){
+  return[{rel: 'stylesheet', href: styles}]
+}
